7d58288f27479e5371f67584297d4720632f9178
GabSalemi <salemi.gabriele94@gmail.com>
2023-07-17 18:25:14 +0200

    fix: useReducer

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -23,20 +23,21 @@ function reducer(state, action) {
 const Hero = () => {
     const sectionData = useContext(DATA)
     const [state, dispatch] = useReducer(reducer, { selected: "none"});
+    console.log(state.selected)
     
 
 return <>
             <div className={styles.hero__section}>
                 <h1>Hero Section</h1>
-                <div className={styles.img__section} onClick={() => {dispatch({ type: 'select_city' })}}>
+                <div className={state.selected === "city" ? styles.section__selected : styles.img__section} onClick={() => {dispatch({ type: 'select_city' })}}>
                     <img src={sectionData.heroImg[0].imgUrl} alt={sectionData.heroImg[0].value} />
                     <h2>{sectionData.heroImg[0].name}</h2>
                 </div>
-                <div className={styles.img__section} onClick={() => {dispatch({ type: 'select_sea' })}}>
+                <div className={state.selected === "sea" ? styles.section__selected : styles.img__section} onClick={() => {dispatch({ type: 'select_sea' })}}>
                     <img src={sectionData.heroImg[1].imgUrl} alt={sectionData.heroImg[1].value} />
                     <h2>{sectionData.heroImg[1].name}</h2>
                 </div>
-                <div className={styles.img__section} onClick={() => {dispatch({ type: 'select_mountain' })}}>
+                <div className={state.selected === "mountain" ? styles.section__selected : styles.img__section} onClick={() => {dispatch({ type: 'select_mountain' })}}>
                     <img src={sectionData.heroImg[2].imgUrl} alt={sectionData.heroImg[2].value} />
                     <h2>{sectionData.heroImg[2].name}</h2>
                 </div>
